7207c300daeac8d9922d41cbc0e28f97fe42fc07
walkercyber <isabella.daood@iths.se>
2025-02-26 22:35:19 +0100

    last commit

--- FullstackLabb3/Models/Skill.cs
+++ FullstackLabb3/Models/Skill.cs
@@ -7,8 +7,6 @@ namespace FullstackLabb3.Models
         public int Id { get; set; }
         public string Technology { get; set; }
         public int Yearsofexperience { get; set; }
-
-        [Range(1, 5)]
-        public int SkillLevel { get; set; }
+        public string SkillLevel { get; set; }
     }
 }
